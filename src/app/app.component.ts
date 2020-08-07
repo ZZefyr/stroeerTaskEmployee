198771aee2e9792ac25e7601cb9ab1e56f47618d
zzefyr <vojkuvka.zdenek@gmail.com>
2020-08-07 06:23:37 +0200

    Header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'stroeerTaskEmployee';
+  title = 'Stroeer Employee App';
 }
